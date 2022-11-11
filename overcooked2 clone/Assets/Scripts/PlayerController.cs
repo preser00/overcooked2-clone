c8f00656757b35b48e220c0060ea7a5d8a238cdc
Jacob <kl3298@nyu.edu>
2022-11-11 08:36:21 -0500

    Trivial Player Movement

--- overcooked2 clone/Assets/Scripts/PlayerController.cs
+++ overcooked2 clone/Assets/Scripts/PlayerController.cs
@@ -12,18 +12,25 @@ public class PlayerController : MonoBehaviour
         
     }
 
-
+    #region Movement Code
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        
-        Debug.Log(movement.x);
-        
-        
+        movement.y = Input.GetAxisRaw("Vertical");
     }
     private void FixedUpdate()
     {
         rigidbody.MovePosition(rigidbody.position + movement * movementSpeed);
+    }
+    #endregion
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Interactable")
+        {
+            //Change State to that action
+        }
+
+        
     }
 
 }
