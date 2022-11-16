6813b6462461026de0ae27e0782ef6661f2890c4
w <jwr8797@nyu.edu>
2022-11-16 10:47:24 -0500

    start of item spawners

---
+++ overcooked2 clone/Assets/Scripts/CounterController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterController : MonoBehaviour
+{
+    public string counterType; //all lowercase strings
+                              //ingredient spawner, plate spawner, cutting board, trash can 
+
+    
+    void Update()
+    {
+        if(counterType == "ingredient spawner")
+        {
+
+        }
+        else if(counterType == "plate spawner")
+        {
+
+        }
+        else if(counterType == "cutting board")
+        {
+
+        }
+        else if(counterType == "trash can")
+        {
+
+        }
+    }
+}
