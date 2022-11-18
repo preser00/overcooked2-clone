227d0f47bca0b7bd15fbef71b550d448f5031e04
w <jwr8797@nyu.edu>
2022-11-18 10:36:50 -0500

    ingredient spawners + trash can

--- overcooked2 clone/Assets/Scripts/TableController.cs
+++ overcooked2 clone/Assets/Scripts/TableController.cs
@@ -4,23 +4,58 @@ using UnityEngine;
 
 public class TableController : MonoBehaviour
 {
-    public string tableType; //all lowercase strings
-                               //ingredient spawner, plate spawner, cutting board, trash can 
-    public int ingredientType; //for ingredient spawning counters
-                               //0 = fish, 1 = shrimp 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public string tableType; //all lowercase strings:
+                             //ingredient spawner, plate spawner, cutting board, trash can, delivery zone 
+    
+    public string ingredientSpawnerType; //for ingredient spawning counters; choices are fish, shrimp
+    
+    public GameObject ingredient; //ingredient template goes here
+    public GameObject plate; //plate template goes here
+    
+    private TableSelector _tableSelector; //player's table selector
+
+    private void Start()
     {
-        
-        if(collision.gameObject.tag == "Player") //if the player is touching a counter, check for key inputs
-                                                  //based on the type of counter the current one is
+        _tableSelector = GameObject.FindGameObjectWithTag("Player").GetComponent<TableSelector>(); //get table selector
+    }
+
+    private void Update()
+    {
+        Debug.Log(_tableSelector.TableSelected);
+
+        OnSelection(); 
+    }
+
+    private void OnSelection()
+    {
+        if (_tableSelector.TableSelected == gameObject) //if this table is selected do stuff based on type of table
         {
+            PlayerController currentPlayerController = _tableSelector.gameObject.GetComponent<PlayerController>(); //get player's controller
+
             if (tableType == "ingredient spawner")
             {
+                if (Input.GetKeyUp(KeyCode.Space)) //getkeyup so it doesn't spawn like 8
+                {
+                    GameObject newIngredient = Instantiate(ingredient, transform.position + new Vector3(1, 1, 0), Quaternion.identity);
+                    IngredientController newIngredientController = newIngredient.GetComponent<IngredientController>(); //get ingredient's IngredientController script
+                    
+                    //WIP - if player is currently already holding ingredient, make sure to put the ingredient somewhere it wont clip into a table. can probably be done in playercontroller
 
+                    currentPlayerController.currentHolding = newIngredient; //set player's currently holding obj to this new ingredient
+
+                    newIngredientController.ingredientName = ingredientSpawnerType; //set name to match spawner type's ingredient (fish, shrimp)
+                    newIngredientController.held = true; //set the ingredient to being held 
+                    newIngredientController.master = _tableSelector.gameObject; //set ingredient's master to the player that selected the table
+                }
             }
             else if (tableType == "plate spawner")
             {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    GameObject newPlate = Instantiate(plate, transform.position + new Vector3(1, 1, 0), Quaternion.identity); 
 
+                    //WIP - need player pickup access
+                }
             }
             else if (tableType == "cutting board")
             {
@@ -28,9 +63,17 @@ public class TableController : MonoBehaviour
             }
             else if (tableType == "trash can")
             {
+                if(Input.GetKeyUp(KeyCode.Space) && currentPlayerController.currentHolding.layer == 7) //if player is holding an ingredient (layer = 7) & presses space
+                {
+                    //destroy ingredient
+                    Destroy(currentPlayerController.currentHolding); 
 
+                }
+            }
+            else if(tableType == "delivery zone")
+            {
+                //add to score, remove plate, etc
             }
         }
-        
     }
 }
