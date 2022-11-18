50ebd5ac9c0cae4637c96ed36c647b02ec09956c
Jacob <kl3298@nyu.edu>
2022-11-18 09:01:08 -0500

    Raycasting Function

--- overcooked2 clone/Assets/Scripts/TableReverter.cs
+++ overcooked2 clone/Assets/Scripts/TableReverter.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class TableReverter : MonoBehaviour
 {
     public Color original;
+    public Color red = Color.red;
+    public bool isSelectedPlayer;
+    public SpriteRenderer spriteRenderer;
+   
     // Start is called before the first frame update
     void Start()
     {
-       original = gameObject.GetComponent<SpriteRenderer>().color; //saving the original color of this table
+        //original = gameObject.GetComponent<SpriteRenderer>().color; //saving the original color of this table
+        original = spriteRenderer.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isSelectedPlayer)
+        {
+            spriteRenderer.color = red;
+        }
+        else
+        {
+            spriteRenderer.color = original;
+        }
     }
     /*public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -24,6 +36,7 @@ public class TableReverter : MonoBehaviour
             gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 0.30196078f, 0.30196078f);
         }
     }*/
+    /*
     public void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -32,4 +45,5 @@ public class TableReverter : MonoBehaviour
             gameObject.GetComponent<SpriteRenderer>().color = original;
         }
     }
+    */
 }
